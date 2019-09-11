77876174e428a36bea33730da83609c83fafbc51
Michael Schwartz <mikethedj4@yahoo.com>
2019-09-11 14:05:15 -0500

    minor adjustment

--- js/script.js
+++ js/script.js
@@ -86,7 +86,7 @@ $("[data-action=disclaimer]").click(function() {
   var msg1 = "I Michael Schwartz developed this workout app for myself and myself only!\n\n",
       msg2 = "I am not held liable if you do any of the workouts listed in this app!\n\n",
       msg3 = "By using this app you agree that you're doing these workouts by your own discression only!<br><br>",
-      msg4 = "Contribution and Source Code: <br><a href=\"https://github.com/michaelsboost/Michaels-Workout-App/\">https://github.com/michaelsboost/Michaels-Workout-App/</a><br>",
+      msg4 = "Contribution and Source Code: <br><a href=\"https://github.com/michaelsboost/Michaels-Workout-App/\" target=\"_blank\">https://github.com/michaelsboost/Michaels-Workout-App/</a><br>",
       msg5 = "<a href=\"https://play.google.com/store/apps/details?id=com.michael.workoutapp&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1\" target=\"_blank\" title=\"Get it on Google Play\"><img src=\"https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png\" style=\"margin: 0 0 -20px 0;\"></a>";
 
   Swal.fire({
